b14fa6301615322c4f1095bb81bc5eb8a206ec81
MarinaYsn <marina@bluelink.ca>
2021-04-23 11:22:23 -0400

    added css file

---
+++ completeproject/src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExpenseItem() {
+    return (
+        <div>
+            <h2>First Component Item</h2> 
+        </div>
+    );
+}
+
+export default ExpenseItem;
